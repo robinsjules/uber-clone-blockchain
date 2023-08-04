1cf4888941db7da30c3d44a8fa0564c997f4efa6
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-04 16:17:40 +0800

    navbar progress

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import Navbar from '../components/Navbar'
+
 const style = {
   wrapper: 'h-screen w-screen flex flex-col',
 }
 export default function Home() {
   return (
     <div className = {style.wrapper}>
-      {/*navbar*/}
+      <Navbar />
       <div className = {style.main}>
         {/*map*/}
       </div>
