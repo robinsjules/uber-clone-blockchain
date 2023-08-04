2de987c8a6744907006a8f8f8e16460f741ad04a
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-04 19:46:24 +0800

    Map update

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+const style = {
+    wrapper:'flex-1 h-full w-full'
+}
+
 const Map = () => {
+    useEffect(()=> {
+        const map = new mapboxgl.Map({
+            style: 'mapbox://drakosi/ckvcwq3rwdw4314o3i2ho8tph'
+        })
+    })
     return <div>Map</div>
 }
 
