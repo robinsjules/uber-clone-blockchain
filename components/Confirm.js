b5482730ed51900d13cb0961cdb31b9d5088ec44
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-04 23:54:53 +0800

    Added Confirm

---
+++ components/Confirm.js
@@ -0,0 +1,26 @@
+"use client";
+
+const style = {
+    wrapper: `flex-1 h-full flex flex-col justify-between`,
+    rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
+    confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
+    confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
+  }
+  
+const Confirm = () => {
+    const storeTripDetails = async() => {}
+    return (
+        <div className = {style.wrapper}>
+            <div className = {style.rideSelectorContainer}></div>
+            <div className = {style.confirmButtonContainer}>
+                <div className = {style.confirmButton}>
+                    <div className = {style.confirmButton} onClick={() => storeTripDetails()}>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Confirm
