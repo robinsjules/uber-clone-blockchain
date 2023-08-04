c5acf5014ce3089d1092b83192cbff8c3ce4b5de
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-04 23:56:37 +0800

    Button rounding

--- components/Confirm.js
+++ components/Confirm.js
@@ -3,8 +3,8 @@
 const style = {
     wrapper: `flex-1 h-full flex flex-col justify-between`,
     rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
-    confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
-    confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
+    confirmButtonContainer: `border-t-2 cursor-pointer z-10`,
+    confirmButton: `bg-black text-white m-4 py-4 text-center text-xl rounded-2xl`,
   }
   
 const Confirm = () => {
